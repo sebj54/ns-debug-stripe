6ca6ffed4c967c70c12891d47b8b17195eb6623f
Sébastien JEAN <contact@sebjean.com>
2021-09-23 15:02:06 +0200

    💄 Add style

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -18,6 +18,14 @@ export default {
 <style scoped lang="scss">
     @import '@nativescript/theme/scss/variables/blue';
 
+    Page {
+        background-color: #f4f9fe;
+
+        .ns-dark & {
+            background-color: #303030;
+        }
+    }
+
     // Custom styles
     .fas {
         @include colorize($color: accent);
